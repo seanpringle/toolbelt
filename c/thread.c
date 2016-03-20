5a8c9a35983a459bed435b6d4959dabfccfda767
Sean Pringle <sean.pringle@gmail.com>
2016-03-21 09:22:13 +1100

    c

--- c/thread.c
+++ c/thread.c
@@ -165,7 +165,7 @@ errorf_multithreaded (char *pattern, ...)
   mutex_lock(&mutex_stderr);
   va_list args;
   va_start(args, pattern);
-  vfprintf(stderr, "thread %lx: ", self);
+  vfprintf(stderr, "thread %lx: ", (uint64_t)self);
   vfprintf(stderr, pattern, args);
   fputc('\n', stderr);
   va_end(args);
